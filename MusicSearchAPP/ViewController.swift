32ff0c0d75ca90ca3f56214eb92cf1d4741366d3
HiroUrata <1712026@i.softbank.jp>
2021-11-12 11:02:42 +0900

    Initial Commit

---
+++ MusicSearchAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicSearchAPP
+//
+//  Created by UrataHiroki on 2021/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
